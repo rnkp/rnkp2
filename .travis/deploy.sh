278a3e8e81f5e475cccb44d2500991db4d8a0211
4life <ovchinnikovpa@gmail.com>
2019-04-14 14:07:26 +0300

    Upd

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,34 +8,14 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@webhost.rnk.party:/var/www/rnk.party"
+    git remote add deploy "travis@rnk.party:/var/www/rnk.party"
     git config user.name "Travis CI"
-    git config user.email "travis@rnk.party"
+    git config user.email "ovchinnikovpa@gmail.com+travis@gmail.com"
 
     # commit compressed files and push it to remote
-    rm -f .gitignore
-    cp .travis/deployignore .gitignore
     git add .
-    git status # debug
-    git commit -m "Deploy compressed files"
-    git push -f deploy HEAD:master
-
-elif [ $TRAVIS_BRANCH == "staging" ] ; then
-
-    # setup ssh agent, git config and remote
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@webhost.rnk.party:/var/www/rnk.party"
-    git config user.name "Travis CI"
-    git config user.email "travis@rnk.party"
-
-    # commit compressed files and push it to remote
-    rm -f .gitignore
-    cp .travis/deployignore .gitignore
-    git add .
-    git status # debug
-    git commit -m "Deploy compressed files"
-    git push -f deploy HEAD:master
+    git commit -m "Deploy"
+    git push --force deploy master
 
 else
 
