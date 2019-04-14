c5f61473f72a07e9898992a41e43b6ff59671799
4life <ovchinnikovpa@gmail.com>
2019-04-14 14:55:06 +0300

    Upd

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,7 +8,7 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@rnk.party:/var/www/rnk.party"
+    git remote add deploy "travis@webhost.rnk.party:/var/www/rnk.party"
     git config user.name "Travis CI"
     git config user.email "travis@rnk.party"
 
