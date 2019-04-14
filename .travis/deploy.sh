45bee5b699fec90dbf07a8bb7812a3bdd0cfd016
4life <ovchinnikovpa@gmail.com>
2019-04-14 15:41:21 +0300

    Upd

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,18 +5,20 @@ set -xe
 
 if [ $TRAVIS_BRANCH == "master" ] ; then
 
-    # setup ssh agent, git config and remote
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "deploy@rnk.party:/var/www/rnk.party"
-    git config user.name "Travis CI"
-    git config user.email "travis@gmail.com"
+    cp -R ./build/* /var/www/rnk.party
 
-    # commit compressed files and push it to remote
-    git add .
-    git status
-    git commit -m "Deploy"
-    git push --force deploy master
+    # setup ssh agent, git config and remote
+    # eval "$(ssh-agent -s)"
+    # ssh-add ~/.ssh/travis_rsa
+    # git remote add deploy "deploy@rnk.party:/var/www/rnk.party"
+    # git config user.name "Travis CI"
+    # git config user.email "travis@gmail.com"
+
+    # # commit compressed files and push it to remote
+    # git add .
+    # git status
+    # git commit -m "Deploy"
+    # git push --force deploy master
 
 else
 
