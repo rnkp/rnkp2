37e0b065809e31e976bb4d6c05593a82dd17a3af
4life <ovchinnikovpa@gmail.com>
2019-06-18 14:30:13 +0300

    PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,52 +1,33 @@
 this.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/css/main.css',
-                '/js/main.js',
-                '/images/bg1.jpg',
-                '/images/bg1_blur.jpg',
-                '/images/gerb_black_big.png',
-                '/images/logo.jpg',
-                '/images/logo-color-big.png',
-                '/images/logo-color-min.png',
-                '/images/logo-black-min.png',
-                '/images/logo_min.png',
-                '/images/page2_img1.png',
-                '/images/russia.png',
-                '/images/articles/Kurskaya.jpg',
-                '/images/articles/Lissner.jpg',
-                '/images/articles/Varyagi.jpg',
-                '/favicon.ico',
-                '/fonts/sf-ui-display-bold.eot',
-                '/fonts/sf-ui-display-bold.ttf',
-                '/fonts/sf-ui-display-bold.woff',
-                '/fonts/sf-ui-display-light.eot',
-                '/fonts/sf-ui-display-light.ttf',
-                '/fonts/sf-ui-display-light.woff',
-                '/fonts/sf-ui-display-medium.eot',
-                '/fonts/sf-ui-display-medium.ttf',
-                '/fonts/sf-ui-display-medium.woff',
-                '/fonts/sf-ui-display-regular.eot',
-                '/fonts/sf-ui-display-regular.ttf',
-                '/fonts/sf-ui-display-regular.woff'
-            ]);
-        })
-    );
+  var indexPage = new Request('index.html');
+  event.waitUntil(
+    fetch(indexPage).then(function(response) {
+      return caches.open('pwabuilder-offline').then(function(cache) {
+        console.log('[PWA Builder] Cached index page during Install'+ response.url);
+        return cache.put(indexPage, response);
+      });
+  }));
 });
 
 this.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).catch(function() {
-        return fetch(event.request);
-    }).then(function(response) {
-        if(!response) return false;
-
-        caches.open('v1').then(function(cache) {
-            cache.put(event.request, response);
+    var updateCache = function(request){
+      return caches.open('pwabuilder-offline').then(function (cache) {
+        return fetch(request).then(function (response) {
+          console.log('[PWA Builder] add page to offline'+response.url)
+          return cache.put(request, response);
         });
-        return response.clone();
-    }).catch(function() {
-        return caches.match('/images/bg1.jpg');
-    }));
+      });
+    };
+    event.waitUntil(updateCache(event.request));
+    event.respondWith(
+      fetch(event.request).catch(function(error) {
+        console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+        return caches.open('pwabuilder-offline').then(function (cache) {
+          return cache.match(event.request).then(function (matching) {
+            var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+            return report
+          });
+        });
+      })
+    );
 });
