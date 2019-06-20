71623fb02b1514bb5da00142941d2b98b16d7537
4life <ovchinnikovpa@gmail.com>
2019-06-20 17:42:33 +0300

    Footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+import logo from '../images/logo-color-min.png';
+import tg from '../images/tg.png';
 
 const Footer = () => (
   <Route
     children={({ location }) => (
       <footer className={location.pathname === '/' ? 'fixed' : ''}>
-        <div className="box">
-          <div className="footer_logo left"></div>
-          <p>Российская народная концептуальная программа. 2018. Почёт и процветание. <Link to="/privacy">Все права сохранены &copy;</Link></p>
-          <div className="clear"></div>
+        <div className="box flex">
+          <div className="logo-text">
+            <img src={logo} alt="Logo" className="img" />
+            <p>Российская народная концептуальная программа<br/><small>© РНКП, 2011–2019</small></p>
+          </div>
+          <p><Link to="/privacy">Правила использования</Link></p>
+        </div>
+
+        <div className="box text-2">
+            <p>
+              <a href="" target="_blank"><img src={tg} alt="Tg" />Telegram</a>
+              <a href="" target="_blank"><img src={tg} alt="Tg" />Github</a>
+              <a href="" target="_blank"><img src={tg} alt="Tg" />YouTube</a>
+            </p>
         </div>
       </footer>
     )}
