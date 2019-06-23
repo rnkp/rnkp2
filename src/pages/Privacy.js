0eaf8f8fba5de10c41ff5504e7debdf07f8bd91a
4life <ovchinnikovpa@gmail.com>
2019-06-23 21:40:53 +0300

    Add links

--- src/pages/Privacy.js
+++ src/pages/Privacy.js
@@ -9,6 +9,9 @@ const Privacy = () => (
     <div className="box">
       <p>Материалы, статьи и программа партии РНКП являются следствием исторического развития российского народа и государства.</p>
       <p>Материалы, статьи и программа партии РНКП являются собственностью и достоянием российского народа и могут свободно использоваться, изменяться или применяться.</p>
+      <p>Если Вы нашли ошибку в тексте или в верстке сайта партии РНКП, пожалуйста сообщите нам об этом в нашем репозитории в <a href="https://github.com/rnkp/rnkp2/issues/new" target="_blank" rel="noopener noreferrer">Github</a></p>
+      <p>Обсудить Программу и задать вопросы можно в группе в <a href="https://t.me/rnkp_chat" target="_blank" rel="noopener noreferrer">Telegram</a></p>
+      <p>Поддержать работу партии РНКП Вы можете через сервис <a href="https://www.patreon.com/rnkp" target="_blank" rel="noopener noreferrer">Patreon</a></p>
     </div>
   </section>
 );
