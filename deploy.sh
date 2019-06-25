2bcd0a0be96ad47f912854d4d9602e026cd23da6
4life <ovchinnikovpa@gmail.com>
2019-06-25 23:44:44 +0300

    Travis test

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -xe
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  eval "$(ssh-agent -s)"
+
+  ssh-add ~/.ssh/id_rsa
+
+  cd build
+
+  git init
+  git remote add origin git@rnk.party:/home/git/rnkp
+  git config --global user.email "build@travis-ci.org"
+  git add .
+  git commit -m 'deploy'
+  git push --force origin master
+else
+  echo "Not deploying, since this branch isn't master."
+fi
