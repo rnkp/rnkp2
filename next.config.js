f354885a5f7174d8b2181b1119824a0752ecd136
4life <ovchinnikovpa@gmail.com>
2021-02-27 01:48:51 +0300

    Add next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  trailingSlash: true
+  trailingSlash: true,
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/program': { page: '/program' },
+      '/program/1': { page: '/program/[slug]' },
+      '/evolution': { page: '/evolution' },
+      '/articles': { page: '/articles' },
+      '/privacy': { page: '/privacy' },
+    }
+  },
 }
