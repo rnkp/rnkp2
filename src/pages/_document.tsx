9a997a91ff61a199dcf8d6157499bd281fdb128c
rnkp <info@rnkp.org>
2024-04-08 17:41:04 +0300

    Add reach text

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,20 @@ export default class MyDocument extends Document {
           `
             } }
           />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "image": "https://rnkp.org/images/logo-color-big.png",
+              "url": "https://rnkp.org",
+              "sameAs": ["https://www.rnkp.org"],
+              "logo": "https://rnkp.org/images/logo-color-min.png",
+              "name": "РНКП",
+              "description": "Сайт партии Российская народная концептуальная программа (РНКП). Партия создана для ликвидации пропасти между народом и властью и для разработки программы развития страны, обеспечивающей поступательное улучшение качества жизни и решение проблем высокого порядка общественной значимости",
+              "email": "info@rnkp.org"
+            }) }}
+          />
         </Head>
         <body>
           <Main />
